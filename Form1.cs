b21a24eafb18c10f9794c9d72111fc061a426838
brenekv.03 <brenekv.03@A321-15>
2022-03-25 09:26:01 +0100

    Vypracování třídy Student

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,17 @@ namespace OOP00
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student stdt0 = new Student();
+            Student stdt = new Student("Luboš");
+            stdt.OznamkujCJL(3);
+            stdt.OznamkujMAT(2);
+            stdt.OznamkuPRG(1);
+            double p = stdt.Prumer();
+            if (p != -1) MessageBox.Show(stdt.GetJmeno() + " má průměr známek: " + p);
+            else MessageBox.Show(stdt.GetJmeno() + " není klasifikován");        
+        }
     }
 }
--- Student.cs
+++ Student.cs
@@ -8,5 +8,41 @@ namespace OOP00
 {
     class Student
     {
+        private string jmeno;
+        private int znamkaMAT;
+        private int znamkaCJL;
+        private int znamkaPRG;
+        public Student()
+        {
+
+        }
+        public Student(string jmeno)
+        {
+            this.jmeno = jmeno;
+        }
+        public string GetJmeno()
+        {
+            return jmeno;
+        }
+        public void OznamkujMAT(int znamka)
+        {
+            if (znamka >= 1 && znamka <= 5) znamkaMAT = znamka;
+        }
+        public void OznamkujCJL(int znamka)
+        { 
+            if (znamka >= 1 && znamka <= 5) znamkaCJL = znamka;
+        }
+        public void OznamkuPRG(int znamka)
+        {
+            if (znamka >= 1 && znamka <= 5) znamkaPRG= znamka;
+        }
+        public double Prumer()
+        {
+            if (znamkaCJL != 0 && znamkaMAT != 0 && znamkaPRG != 0)
+            {
+                return (double)(znamkaCJL + znamkaMAT + znamkaPRG) / 3;
+            }
+            else return -1;
+        }
     }
 }
